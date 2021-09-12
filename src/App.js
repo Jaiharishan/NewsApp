530c5b1f378c2736a1f0ce3eb94b3b3a209b7288
Jaiharishan <downdkar@gmail.com>
2021-09-12 09:07:28 +0530

    card made

--- src/App.js
+++ src/App.js
@@ -5,28 +5,42 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 
+
 function App() {
 
-  const [news, setNews] = useState('');
+  const [recentNews, setRecentNews] = useState('');
+  const [businessnews, setBusinessNews] = useState('');
+  const [sportsnews, setSportsNews] = useState('');
+  const [technews, setTechNews] = useState('');
+  const [entertainmentnews, setEntertainmentNews] = useState('');
 
   useEffect(() => {
-    const loadNews = () => {
-      axios.get(`https://gnews.io/api/v4/top-headlines?lang=en&topic=business&token=${Api.key}`).then(res => {
-        setNews(res.data.articles);
+    const loadNews = (url, func) => {
+      axios.get(url).then(res => {
+        func(res.data.articles);
       })
     };
+    loadNews(`https://gnews.io/api/v4/top-headlines?lang=en&token=${Api.key}`, setRecentNews);
 
-    
-    loadNews();
+    loadNews(`https://gnews.io/api/v4/top-headlines?lang=en&topic=business&token=${Api.key}`, setBusinessNews);
+
+    loadNews(`https://gnews.io/api/v4/top-headlines?lang=en&topic=sports&token=${Api.key}`, setSportsNews);
+
+    loadNews(`https://gnews.io/api/v4/top-headlines?lang=en&topic=technology&token=${Api.key}`, setTechNews);
+
+    loadNews(`https://gnews.io/api/v4/top-headlines?lang=en&topic=entertainment&token=${Api.key}`, setEntertainmentNews);
   }, [])
 
 
     return (
         <div>
-            <Button>News Application</Button>
 
-            <NewsList news = {news} />
-            
+            <NewsList header = {'Recent News'} news = {recentNews} />
+            <NewsList header = {'Business'} news = {businessnews} />
+            <NewsList header = {'Sports'} news = {sportsnews} />
+            <NewsList header = {'Tech'} news = {technews} />
+            <NewsList header = {'Entertainment'} news = {entertainmentnews} />
+
         </div>
       )
 }
